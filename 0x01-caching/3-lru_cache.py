9310422f34d6bcdd922c02409d9f448c849169cb
Brian Omondi <omondiibrian00@gmail.com>
2024-01-31 21:38:45 +0300

    Lru caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" LRU caching system """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Implement the LRU caching system
+    Removes the last used item
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, value):
+        """ assign to the dictionary self.cache_data the item
+        value for the key key """
+        if key is not None and value is not None:
+            self.cache_data[key] = value
+            self.cache_data.move_to_end(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.cache_data.popitem(last = False)
+
+    def get(self, key):
+        if key not in self.cache_data:
+            return -1
+        else:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
