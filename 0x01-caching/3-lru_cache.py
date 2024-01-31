628d6fd084af147b96b50bb2aa8974f50cd6c0f9
Brian Omondi <omondiibrian00@gmail.com>
2024-01-31 22:05:14 +0300

    Update Lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ LRU caching system """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
@@ -10,20 +11,20 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ Initialize the class """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, value):
         """ assign to the dictionary self.cache_data the item
         value for the key key """
         if key is not None and value is not None:
             self.cache_data[key] = value
-            self.cache_data.move_to_end(key)
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 self.cache_data.popitem(last = False)
+                print('DISCARD: {}'.format(key))
 
     def get(self, key):
         """ Return the value in self.cache_data linked to key """
         if key not in self.cache_data:
-            return -1
+            return None
         else:
-            self.cache_data.move_to_end(key)
             return self.cache_data[key]
