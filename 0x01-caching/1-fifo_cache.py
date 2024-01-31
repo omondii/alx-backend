d8cb8884fc88b9f1e576ef23bf9f557fe4198d25
Brian Omondi <omondiibrian00@gmail.com>
2024-02-01 01:53:07 +0300

    Update on fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,15 +10,18 @@ class FIFOCache(BaseCaching):
         super().__init__()
         self.cache_data = OrderedDict()
 
-    def put(self, key,item):
+    def put(self, key, item):
         """ Assign to the dictionary self.cache_data the item
         value for the key key"""
         if key is not None and item is not None:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                key, value = self.cache_data.popitem(last=False)
-                print('DISCARD: {}'.format(key))
+                key, _ = self.cache_data.popitem(last=False)
+                print(f'Discard: {key}')
 
     def get(self, key):
         """ Return the value in self.cache_data linked to key """
-        return self.cache_data.get(key, None)
+        if key is None:
+            return None
+        else:
+            return self.cache_data[key]
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -24,3 +24,11 @@ General
     What LFU means
     What the purpose of a caching system
     What limits a caching system have
+
+
+A caching system - a mechanism that stores frequently accessed data in a cache(storage space).
+A cache is much faster to access than the original data source hence reduced response times. 
+<u> Caching Systems </u>
+-> Refers to a component used to store copies of frequently accessed data in a cache. 
+1. <b> FIFO </b>
+-> A cache replacement mechanism where the oldest items are removed first when the cache reaches its max capacity
