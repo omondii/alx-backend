41a89cdfe291e5cf8ebbf6554b0e9ebef38cb346
Brian Omondi <omondiibrian00@gmail.com>
2024-01-31 21:12:37 +0300

    updated get function

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 """ A FIFOCaching system """
 from base_caching import BaseCaching
-from collections import OrderedDict
-
 
 class LIFOCache(BaseCaching):
     """ A caching system implemented using FIFO """
     def __init__(self):
         """ Initialize the class """
         super().__init__()
-        self.cache_data = OrderedDict()
 
-    def put(self, key,item):
+    def put(self, key, item):
         """ Assign to the dictionary self.cache_data the item
         value for the key key"""
         if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = list(self.cache_data.keys())[-1]
+                print('DISCARD: {}'.format(discarded_key))
+                del self.cache_data[discarded_key]
+
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                key, value = self.cache_data.popitem(last=True)
-                print('DISCARD: {}'.format(key))
 
     def get(self, key):
         """ Return the value in self.cache_data linked to key """
-        return self.cache_data(key, None)
+        return self.cache_data[key] if key in self.cache_data else None
