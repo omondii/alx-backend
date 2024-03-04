412c0d8318c93770534b0c0e45911baff2207b30
Brian Omondi <omondiibrian00@gmail.com>
2024-03-04 16:11:17 +0300

    setting up redis

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+/**
+ * connects to redis server on local machine
+ */
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+client.on('error', err => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
